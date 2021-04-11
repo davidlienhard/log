3314900f37aba8666586923dd637e5d0bd666edb
David Lienhard <43143441+davidlienhard@users.noreply.github.com>
2021-04-11 14:28:25 +0200

    use davidlienhard/coding-standard (#31)
    use coding-standard for phpcs from `davidlienhard/coding-standard`

--- src/Stub.php
+++ src/Stub.php
@@ -27,33 +27,29 @@ class Stub implements LogInterface
 {
     /**
      * file to save the logs
-     * @var             string
      */
-    private $file;
+    private string $file;
 
     /**
      * whether to use gz compression
-     * @var             bool
      */
-    private $gz;
+    private bool $gz;
 
     /**
      * whether to append data to the file
-     * @var             bool
      */
-    private $append;
+    private bool $append;
 
     /**
      * list of errors
      * @var             array
      */
-    private $errors = [ ];
+    private array $errors = [];
 
     /**
      * stay silent or print the errors to stdout
-     * @var             bool
      */
-    private $silent = false;
+    private bool $silent = false;
 
 
 
@@ -89,11 +85,10 @@ class Stub implements LogInterface
      * @param           string          $text   the line to write
      * @param           bool            $nl     create a newline after the text
      * @param           bool            $date   add thge date before the text
-     * @return          bool
      */
     public function write(string $text, bool $nl = true, bool $date = true) : bool
     {
-        echo ($date ? date("d.m.y H:i:s")." " : "") . $text . ($nl ? "\n" : "");
+        echo ($date ? date("d.m.y H:i:s")." " : "").$text.($nl ? "\n" : "");
 
         return true;
     }
@@ -105,7 +100,6 @@ class Stub implements LogInterface
      * @version         1.0.1, 30.11.2020
      * @since           1.0.1, 30.11.2020, created
      * @copyright       tourasia
-     * @return          bool
      */
     public function close() : bool
     {
@@ -120,7 +114,6 @@ class Stub implements LogInterface
      * @since           1.0.1, 30.11.2020, created
      * @copyright       tourasia
      * @param           bool            $silent         state of the silent flag
-     * @return          void
      * @uses            self::$silent
      */
     public function silent(bool $silent = true) : void
